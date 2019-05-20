601db01779f5a25ea60f59cfb749bb8c3e313575
Mathias Nedrebø <mathias.nedrebo@zividlabs.com>
2019-05-20 22:40:33 +0200

    Added shellcheck for Bash scripts

--- continuous-integration/lint.sh
+++ continuous-integration/lint.sh
@@ -5,7 +5,11 @@ ROOT_DIR=$(realpath "$SCRIPT_DIR/..")
 
 bashFiles=$(find "$ROOT_DIR" -name '*.sh')
 
+echo Running shellcheck on:
+echo "$bashFiles"
+shellcheck -x -e SC1090,SC2086,SC2046 $bashFiles || exit $?
+
 echo Running code analysis on C++ code:
 $SCRIPT_DIR/lint-cpp.sh || exit $?
 
-echo Success! ["$0"]
+echo Success! ["$(basename $0)"]
---
+++ continuous-integration/test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo Success! ["$(basename $0)"]
